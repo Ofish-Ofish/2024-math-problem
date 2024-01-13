f7da64c4f2fbc9e3a937b992dfbc9107dec5b09a
Adamimoka <adamimoka@gmail.com>
2024-01-12 23:52:01 -0500

    Combined with my branch. Copied all previous solutions.

--- 2024.py
+++ 2024.py
@@ -1,58 +1,74 @@
-from math import factorial, prod
-from time import time, strftime
-from random import getrandbits, shuffle, randint
+from math import factorial, prod, sqrt
+from time import time
+from random import getrandbits, shuffle
 from functools import cache
 
 start_time = time()
 
 @cache
 def fac(a):
+    if a > 100 or type(a) is not int: return
     return factorial(a)
 
 @cache
-def dbfac(n):
-    start = 2 if n % 2 == 0 else 1
-    return prod(range(start, n+1, 2))
+def dbfac(a):
+    if a > 100 or a < 0 or type(a) is not int: return
+    start = 2 if a % 2 == 0 else 1
+    return prod(range(start, a+1, 2))
 
 @cache
 def sq(a):
-    return a ** 0.5
+    if a < 0: return #Theres a chance a non-intiger could be used, so we might have to remove this.
+    return sqrt(a)
 
 @cache
-def EVALL(a):
+def e(a):
     return eval(a, {"f": fac, "s": sq, 'd': dbfac})
     
 nums = ['2','0','2','4']
 ops = ['','f','d','s']
 signs = ['+','-','*','/','**',  '+','-','*']
 
-looking_for = list(range(1, 101))
-# missing_nums = [57,69,71,73,75,77,91,93,]
-found_nums = []
+#Import already found solutions from numsolver_solutions.txt
+found_expressions = {}
+with open(f'numsolver_solutions.txt', 'r') as f:
+    expressions = f.readlines()
+    expressions.pop(0) #Remove header line
+    for expression in expressions:
+        solution, val = expression.split(' = ')
+        found_expressions[int(val)] = solution
+#Done importing solutions
+looking_for = list(range(101))
 trycount = 0
+done = False
 try:
-    while True:
+    while not done:
         trycount += 1
 
         shuffle(nums)
         if not getrandbits(5):
-            #randomly concatinate random pairs of numbers, or triplets
+            #randomly concatinate random pairs or triplets or all 4
             if not getrandbits(2):
                 newnums = [nums[0]+nums[1]+nums[2], nums[3]]
             elif getrandbits(1):
                 newnums = [nums[0]+nums[1], nums[2], nums[3]]
-            else:
+            elif getrandbits(2):
                 newnums = [nums[0]+nums[1], nums[2]+nums[3]]
+            else:
+                newnums = [nums[0]+nums[1]+nums[2]+nums[3]]
             shuffle(newnums)
         else:
             newnums = nums.copy()
         
         for num in newnums:
-            num = ops[getrandbits(2)] + '(' + ('.' if getrandbits(2) else '') + num + ')'
+            if getrandbits(1): #Small chance to not use parenthesis/functions
+                num = ops[getrandbits(2)] + '(' + ('.' if getrandbits(2) else '') + num + ')'
+            
         
         for i in range(len(newnums)):
-            newnums[i] = ops[getrandbits(2)] + '(' + newnums[i]
-            newnums[i+randint(0,len(newnums)-1-i)] += ')'
+            if getrandbits(1): #Small chance to not use parenthesis/functions
+                newnums[i] = ops[getrandbits(2)] + '(' + newnums[i]
+                newnums[(getrandbits(2))%(len(newnums)-i)+i] += ')'
         
         for i in range(len(newnums)):
             if i + 1 == len(newnums):
@@ -60,10 +76,10 @@ try:
             newnums[i] += signs[getrandbits(3)]
         
         expression = ''.join(newnums)
-        val = 0
+        _ = 0
         
         try:
-            val = EVALL(expression)
+            val = e(expression)
             if(val - int(val) < 0.001):
                 val = int(val)
             else:
@@ -74,22 +90,19 @@ try:
             continue
 
         if val in looking_for:
-            looking_for.remove(val)
-            found_nums.append(f"{expression}  =   {str(val)}")
-            print('found', val, 'with:', expression, '           after ' + str(trycount) + ' tries and ' + str(round(time() - start_time, 2)) + ' seconds. Amount left: ' + str(len(looking_for)))
-            if not looking_for:
-                print('found all numbers after ' + str(trycount) + ' tries and ' + str(round(time() - start_time, 2)) + ' seconds!')
-                #Export solutions to file, in numerical order. Also export tries and time taken.
-                found_nums.sort(key=lambda x: int(x.split(' = ')[1]))
-                with open(f'solutions/numsolver_solutions{strftime("%Y-%m-%d %H:%M:%S")}.txt', 'w') as f:
-                    f.write('Solutions found after ' + str(trycount) + ' tries and ' + str(round(time() - start_time, 2)) + ' seconds!\n')
-                    for solution in found_nums:
-                        f.write(solution + '\n')
-                break
+            if val not in found_expressions.keys():
+                found_expressions[val] = expression
+                print('new solution found', val, '=', expression, '           after ' + str(trycount) + ' tries and ' + str(round(time() - start_time, 2)) + ' seconds.')
+            elif len(expression) < len(found_expressions[val]): #Solution has already been found, see if smaller.
+                found_expressions[val] = expression
+                print('shorter solution found', val, '=', expression, '           after ' + str(trycount) + ' tries and ' + str(round(time() - start_time, 2)) + ' seconds. Amount left: ' + str(len(looking_for)))
 except KeyboardInterrupt:
-    found_nums.sort(key=lambda x: int(x.split(' = ')[1]))
-    with open(f'solutions/numsolver_solutions{strftime("%Y-%m-%d %H-%M-%S")}.txt', 'w') as f:
-        f.write(str(len(found_nums))+' solutions found after ' + str(trycount) + ' tries and ' + str(round(time() - start_time, 2)) + ' seconds!\n')
-        for solution in found_nums:
-            f.write(solution + '\n')
-print(found_nums)
+    done = True
+
+with open(f'numsolver_solutions.txt', 'w') as f:
+    f.write(("ALL!! (🙂🙂🙂)" if len(found_expressions.keys()) == len(looking_for) else str(len(found_expressions))) + ' solutions found after ' + str(trycount) + ' tries and ' + str(round(time() - start_time, 2)) + ' seconds!\n')
+    #Sort found_expressions by their keys
+    found_expressions = {key: value for key, value in sorted(found_expressions.items())}
+    for val, expression in sorted(found_expressions.items()):
+        f.write(expression + " = " + str(val)+'\n')
+print("Successfullly saved solutions to numsolver_solutions.txt")
